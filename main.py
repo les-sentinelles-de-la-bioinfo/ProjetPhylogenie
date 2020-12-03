a3c1fa3164c1970f911871da3372b279fad84699
lola-denet <56269443+lola-denet@users.noreply.github.com>
2020-12-03 16:55:21 +0100

    update sauvegardes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import time
+import random
+import string
 from Bio import Entrez, AlignIO, SeqIO, Phylo
 from Bio.Align.Applications import ClustalwCommandline, MuscleCommandline
 from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor
@@ -22,21 +24,26 @@ parent_path = os.path.dirname(current_path)
 id_list = ["MT298507.1", "HQ954792.1", "MK013995.1", "KJ128666.1", "JX218056.1", "KP403684.1", "HQ536294.1",
            "MK013988.1"]
 
+def get_random_string(length):
+    """Générer une chaîne aléatoire de longueur fixe"""
+    str = string.ascii_lowercase
+    return ''.join(random.choice(str) for i in range(length))
+
 def get_fasta(id_list):
     # write a fasta file for each gene
     filename_list = []
     for gene_id in id_list:
         records = Entrez.efetch(db="nucleotide", id=gene_id, rettype="fasta", retmode="text")
         filename = (gene_id + '.fasta').format(records)
-        filename_list.append("static/data/sauvegardes/" + filename)
+        filename_list.append("static/data/sauvegardes/" + dirName + filename)
         print('Writing:{}'.format(filename))
         time.sleep(1)
-        with open('static/data/sauvegardes/' + filename, 'w') as f:
+        with open('static/data/sauvegardes/' + dirName + filename, 'w') as f:
             f.write(records.read())
             f.close()
 
     # combine files in multifasta file
-    with open('static/data/sauvegardes/multifasta.fasta', 'w') as outfile:
+    with open('static/data/sauvegardes/'+ dirName + 'multifasta.fasta', 'w') as outfile:
         for fname in filename_list:
             with open(fname) as infile:
                 outfile.write(infile.read())
@@ -52,8 +59,8 @@ def clustal_alignment(infile, outfile):
     if (user_OS == 'win32'):
         clustal_exe = current_path + "/static/tools/Windows/clustal-omega-1.2.2-win64/clustalo.exe"
 
-    cline = ClustalwCommandline(clustal_exe, infile="static/data/sauvegardes/" + infile,
-                                outfile="static/data/sauvegardes/" + outfile)
+    cline = ClustalwCommandline(clustal_exe, infile="static/data/sauvegardes/" + dirName + infile,
+                                outfile="static/data/sauvegardes/" + dirName + outfile)
     stdout, stderr = cline()
 
 def muscle_alignment(infile, outfile):
@@ -66,14 +73,14 @@ def muscle_alignment(infile, outfile):
     if(user_OS == 'win32'):
         muscle_exe = current_path + "/static/tools/Windows/muscle3.8.31_i86win32.exe"
 
-    in_file = "static/data/sauvegardes/" + infile
-    out_file = "static/data/sauvegardes/" + outfile
+    in_file = "static/data/sauvegardes/" + dirName + infile
+    out_file = "static/data/sauvegardes/" + dirName + outfile
     muscle_cline = MuscleCommandline(muscle_exe, input=in_file, out=out_file)
     stdout, stderr = muscle_cline()
 
 def NJ_tree(infile, file_type):
     #Tree creation with neighbor-joining
-    filename = "static/data/sauvegardes/" + infile
+    filename = "static/data/sauvegardes/" + dirName + infile
     aln = AlignIO.read(filename, file_type) #clustal si alignement clustal, fasta si alignement fasta
     calculator = DistanceCalculator('identity')
     constructor = DistanceTreeConstructor(calculator, 'nj') # nj ou UPGMA
@@ -83,7 +90,7 @@ def NJ_tree(infile, file_type):
     #Phylo.draw_ascii(tree)
     tree.ladderize()
     Phylo.draw(tree, do_show=False)
-    foo = current_path + '/static/figure/tree.png'
+    foo = current_path + '/static/figure/' + dirName + 'tree.png'
     plt.savefig(foo)
 
 
@@ -94,25 +101,27 @@ def ML_tree(infile, outfile, file_type):
     # phylogeny page should allow to choose maximum lieklihood method
 
     # convert file to phylip
-    records = SeqIO.parse("static/data/sauvegardes/" + infile, file_type)  # clustal <-> fasta
-    count = SeqIO.write(records, "static/data/sauvegardes/" + outfile + ".phylip", "phylip")
+    records = SeqIO.parse("static/data/sauvegardes/" + dirName + infile, file_type)  # clustal <-> fasta
+    count = SeqIO.write(records, "static/data/sauvegardes/" + dirName + outfile + ".phylip", "phylip")
     print("Converted %i records" % count)
 
     if (user_OS == 'darwin'):
         cmd = PhymlCommandline(cmd='static/tools/MacOS/PhyML-3.1/PhyML-3.1_macOS-MountainLion',
-                               input='static/data/sauvegardes/' + outfile + '.phylip')
+                               input='static/data/sauvegardes/' + dirName + outfile + '.phylip')
     if (user_OS == 'linux'):
         cmd = PhymlCommandline(cmd='static/tools/Linux/PhyML-3.1/PhyML-3.1_linux64',
-                               input='static/data/sauvegardes/' + outfile + '.phylip')
+                               input='static/data/sauvegardes/' + dirName + outfile + '.phylip')
     if (user_OS == 'win32'):
         cmd = PhymlCommandline(cmd= current_path + '/static/tools/Windows/PhyML-3.1/PhyML-3.1_win32.exe',
-                               input='static/data/sauvegardes/' + outfile + '.phylip')
+                               input='static/data/sauvegardes/' + dirName + outfile + '.phylip')
 
     out_log, err_log = cmd()
-    tree = Phylo.read('static/data/sauvegardes/' + outfile + '.phylip_phyml_tree.txt', 'newick')
+    tree = Phylo.read('static/data/sauvegardes/' + dirName + outfile + '.phylip_phyml_tree.txt', 'newick')
     Phylo.draw(tree, do_show=False)
-    foo = current_path + '/static/figure/tree.png'
+    foo = current_path + '/static/figure/' + dirName + 'tree.png'
     plt.savefig(foo)
+
+dirName = get_random_string(10) + "/"
 
 # get_fasta(id_list)
 # clustal_alignment("multifasta.fasta","msa_clustal.fasta")
